978e14bbcc74019ae7b998b1f23027ac942f25c8
adryanbartolome <adryanbartolome8123@gmail.com>
2024-04-01 18:12:28 -0700

    Updated the code to encapsulate in a function

--- index.js
+++ index.js
@@ -10,29 +10,31 @@ const froyoOrder = froyoOrderString.split(","); // creates an array of ["vanilla
 
 console.log(froyoOrder)
 
-// create empty  object to store the order count
-let orderCount = {}
 
-for (let i = 0; i < froyoOrder.length; i++) {
-    let currentOrder = froyoOrder[i];
-    if (currentOrder in orderCount) {
-        orderCount[currentOrder] += 1;
-    } else {
-        orderCount[currentOrder] = 1;
-    }
+function countFroyoOrder() {
+    // create empty  object to store the order count
+    let orderCount = {}
+    for (let i = 0; i < froyoOrder.length; i++) {
+        let currentOrder = froyoOrder[i];
+        if (currentOrder in orderCount) {
+            orderCount[currentOrder] += 1;
+        } else {
+            orderCount[currentOrder] = 1;
+        }
+    } return orderCount;
 }
 
 
-// alt answer with a for...of loop
-
-// const orderCount = {};
-
-// for (let order of froyoOrder) {
-//     if (orderCount[order]) {
-//         orderCount[order] += 1;
-//     } else {
-//         orderCount[order] = 1;
-//     }
+// alt answer when using a for...of loop
+// function countFroyoOrder() {
+//     const orderCount = {};
+//     for (let order of froyoOrder) {
+//         if (orderCount[order]) {
+//             orderCount[order] += 1;
+//         } else {
+//             orderCount[order] = 1;
+//         }
+//     } return orderCount;
 // }
 
-console.log(orderCount);
+console.log(countFroyoOrder());
